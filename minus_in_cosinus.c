0231e03dec426d0415ecdcc5a18590b1fb097ceb
Thomas LE MIGNAN <tle-mign@student.42.fr>
2014-01-13 20:01:09 +0100

    Create minus_in_cosinus.c

---
+++ minus_in_cosinus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minus_in_cosinus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-mign <tle-mign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/13 19:29:35 by tle-mign          #+#    #+#             */
+/*   Updated: 2014/01/13 19:40:33 by tle-mign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libtrigonometry.h"
+
+float	ft_minus_in_cosinus(float a, float b)
+{
+	float	result;
+
+	if (a == 0 || b == 0)
+		return (0);
+	result = cos(a) * cos(b) + sin(a) * sin(b);
+	return (result);
+}
